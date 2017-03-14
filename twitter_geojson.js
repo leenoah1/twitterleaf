c66861a15d48ef4b7949b6916c7276545aa9b062
Noah/ <leenoah1@umbc.edu>
2017-03-14 16:49:10 -0400

    Create twitter_geojson.js

---
+++ twitter_geojson.js
@@ -0,0 +1,47 @@
+{
+    "type": "FeatureCollection", 
+    "features": [
+        {
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [
+                    -77.00889706, 
+                    38.90019619
+                ]
+            }, 
+            "type": "Feature", 
+            "properties": {
+                "text": "@work", 
+                "created_at": "Wed Mar 08 15:37:24 +0000 2017"
+            }
+        }, 
+        {
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [
+                    -77.20177438, 
+                    39.09414976
+                ]
+            }, 
+            "type": "Feature", 
+            "properties": {
+                "text": "Leaving class, finally", 
+                "created_at": "Wed Mar 08 01:06:48 +0000 2017"
+            }
+        }, 
+        {
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [
+                    -77.20183901, 
+                    39.094163
+                ]
+            }, 
+            "type": "Feature", 
+            "properties": {
+                "text": "Tweet tweet", 
+                "created_at": "Wed Mar 08 00:38:25 +0000 2017"
+            }
+        }
+    ]
+}
