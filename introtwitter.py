91f4e81c8c980cf5e89f7baad514498fb3d5f628
Noah/ <leenoah1@umbc.edu>
2017-03-14 20:42:52 -0400

    Update introtwitter.py

--- introtwitter.py
+++ introtwitter.py
@@ -59,6 +59,7 @@ with open(tweets_filename) as data_file:
 				"type": "Feature",
 				"properties": {
 					"twitter_handle": tweet['user']['screen_name'],
+					"name": tweet['user']['name'],
 					"text": tweet['text']
 				},
 				"geometry": {
