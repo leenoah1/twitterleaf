0bb84cdcc447f69e39334cd374a4af94b7607c48
Noah/ <leenoah1@umbc.edu>
2017-03-17 13:18:33 -0400

    Update twitter2_geotag3.js

--- twitter2_geotag3.js
+++ twitter2_geotag3.js
@@ -14,7 +14,7 @@ var tweets2 = {
                 "text": "Life is great", 
                 "twitter_handle": "geotag3", 
                 "name": "Geo"
-                "image": "https://upload.wikimedia.org/wikipedia/commons/7/77/Kas_Flowers.JPG"
+                "image": "https://pbs.twimg.com/media/C65zqF4WgAEELhf.jpg"
             }
         },        
         {
