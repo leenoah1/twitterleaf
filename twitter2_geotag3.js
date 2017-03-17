32acf6420dc6b2db2c7c77eb41d2cdbf339633f3
Noah/ <leenoah1@umbc.edu>
2017-03-17 13:21:10 -0400

    Update twitter2_geotag3.js

--- twitter2_geotag3.js
+++ twitter2_geotag3.js
@@ -12,7 +12,7 @@ tweets2 = {
             "type": "Feature", 
             "properties": {
                 "text": "Finally, some white in our winter lives! https://t.co/Fuj0MhU4VQ", 
-                "twitter_handle": "geotag3", 
+                "twitter_handle": "@geotag3", 
                 "name": "geot",
 		"image": "https://pbs.twimg.com/media/C65zqF4WgAEELhf.jpg"
             }
